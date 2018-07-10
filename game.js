019275d554102384d034f513035a4cc4535d3f41
WinstonItiDev <winstonshred@gmail.com>
2018-07-10 17:01:10 +1200

    commit Event-driven DOM Modification

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 // Don't change or delete this line! It waits until the DOM has loaded, then calls 
 // the start function. More info: 
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
@@ -12,6 +13,7 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue);
   }
 }
 
@@ -22,6 +24,11 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
 
