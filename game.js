b9e708534b0dc1e6501d235b0a8fb93f32881dc9
WinstonItiDev <winstonshred@gmail.com>
2018-07-11 15:42:05 +1200

    3rd commit event-driven DOM modification

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue);
+    dots[i].addEventListener('dblclick', hide);
   }
 }
 
@@ -31,6 +32,12 @@ function makeBlue (evt) {
 }
 
 // CREATE FUNCTION hide HERE
+
+function hide(evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+
+}
 
 function updateCounts () {
   var totals = {
